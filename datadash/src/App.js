f06315e078405bba46410819e35870bf8331c266
Ryan Birmingham <birm@rbirm.us>
2018-08-17 16:50:51 -0400

    get method

--- datadash/src/App.js
+++ datadash/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+d3Selection = require('d3-selection');
+
 class App extends Component {
   render() {
     return (
--- datadash/src/visualizations/filters/BarChart.js
+++ datadash/src/visualizations/filters/BarChart.js
@@ -4,13 +4,28 @@ class BarChart extends React.Component{
     let id = props.id
     let bins = props.bins || 5
     let col = props.col
-    this.state = {id: id, bins:bins, col:col}
+    this.state = {id: id, bins:bins, col:col, loaded:false, data=[]}
+  }
+
+  componentDidMount() {
+    store.matrix([this.state.col]).then((data)=>{
+      // translate
+      this.state.loaded=true
+      this.state.data = data
+    }, (error)=>{
+      this.state.loaded = []
+      console.warn(error)
+    })
   }
 
   render(){
-    return(
-      <div className="barchart graph filter" id={this.props.id}>
-      </div>
-    )
+    if (this.state.loaded){
+      return(
+        <div className="barchart graph filter" id={this.props.id+"-chart"}>
+        </div>
+      )
+    } else {
+      return(<div className="barchart graph filter" id={this.props.id+"-chart"}>Load Failed</div>)
+    }
   }
 }
